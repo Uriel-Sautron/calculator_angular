b9f7807726f0025e4b3eb1bda6eb38ad21b2e93e
Uriel SAUTRON <uriel.sautron@gmail.com>
2022-10-24 22:15:41 +0200

    Basic operation ok

--- src/app/keyboard/keyboard.component.ts
+++ src/app/keyboard/keyboard.component.ts
@@ -9,7 +9,83 @@ import { BUTTON } from '../mock-buttons';
 export class KeyboardComponent implements OnInit {
   constructor() {}
 
+  inputKey: string = '';
+  currentNumber: number = 0;
+  operator: string = '';
+
   ngOnInit(): void {}
+
+  add(number1: number, number2: number): void {
+    this.currentNumber = number1 + number2;
+  }
+
+  sub(number1: number, number2: number): void {
+    this.currentNumber = number1 - number2;
+  }
+
+  multi(number1: number, number2: number): void {
+    this.currentNumber = number1 * number2;
+  }
+
+  div(number1: number, number2: number): void {
+    this.currentNumber = number1 / number2;
+  }
+
+  calcul(operator: string) {
+    switch (operator) {
+      case '+':
+        this.add(this.currentNumber, Number(this.inputKey));
+        break;
+      case '-':
+        this.sub(this.currentNumber, Number(this.inputKey));
+        break;
+      case 'X':
+        this.multi(this.currentNumber, Number(this.inputKey));
+        break;
+      case '÷':
+        this.div(this.currentNumber, Number(this.inputKey));
+        break;
+      default:
+        this.currentNumber = Number(this.inputKey);
+        break;
+    }
+
+    this.inputKey = '';
+  }
+
+  onClick(label: string): void {
+    switch (label) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+      case '.':
+        this.inputKey += label;
+        break;
+      case '+':
+      case '-':
+      case '÷':
+      case 'X':
+        this.calcul(this.operator);
+        this.operator = label;
+        break;
+      case '=':
+        this.calcul(this.operator);
+        break;
+      case 'AC':
+        this.currentNumber = 0;
+        this.inputKey = '';
+        this.operator = '';
+        break;
+    }
+    console.log('inputKey:', this.inputKey);
+  }
 
   buttons = BUTTON;
 }
