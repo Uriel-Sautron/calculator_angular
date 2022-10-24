a72fbf716ebeb66e67f578dd5dd2ed4d3f42e468
Uriel SAUTRON <uriel.sautron@gmail.com>
2022-10-24 23:14:37 +0200

    Add app-button in keyboard

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter } from '@angular/core';
+import { Button } from '../mock-buttons';
 
 @Component({
   selector: 'app-button',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
   constructor() {}
+
+  @Input() button?: Button;
+  @Input() onClick?: EventEmitter<void>;
 
   ngOnInit(): void {}
 }
